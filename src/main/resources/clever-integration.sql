49f458006093037457965679b5086e75a994d282
daowangli <daowangli@gmail.com>
2014-07-25 01:40:24 +0800

    #1 Framework

---
+++ src/main/resources/clever-integration.sql
@@ -0,0 +1,111 @@
+USE [master]
+GO
+
+/****** Object:  Database [clever-integration]    Script Date: 2014/7/25 1:38:21 ******/
+CREATE DATABASE [clever-integration]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'clever-integration', FILENAME = N'J:\DB\clever-integration.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'clever-integration_log', FILENAME = N'J:\DB\clever-integration_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [clever-integration] SET COMPATIBILITY_LEVEL = 120
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [clever-integration].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [clever-integration] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [clever-integration] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [clever-integration] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [clever-integration] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [clever-integration] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [clever-integration] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [clever-integration] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [clever-integration] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [clever-integration] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [clever-integration] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [clever-integration] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [clever-integration] SET  READ_WRITE 
+GO
+
